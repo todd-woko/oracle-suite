45e95917583f0d43acabd3f3edbba072754339d3
Michał Dobaczewski <mdobak@gmail.com>
2023-08-15 15:30:37 +0200

    Feed hello message [sc-2844] (#198)

---
+++ pkg/transport/messages/greet.go
@@ -0,0 +1,41 @@
+//  Copyright (C) 2020 Maker Ecosystem Growth Holdings, INC.
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Affero General Public License as
+//  published by the Free Software Foundation, either version 3 of the
+//  License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Affero General Public License for more details.
+//
+//  You should have received a copy of the GNU Affero General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package messages
+
+import (
+	"github.com/defiweb/go-eth/types"
+)
+
+const GreetV1MessageName = "greet/v1"
+
+type Greet struct {
+	Signature types.Signature
+}
+
+// MarshallBinary implements the transport.Message interface.
+func (e *Greet) MarshallBinary() ([]byte, error) {
+	return e.Signature.Bytes(), nil
+}
+
+// UnmarshallBinary implements the transport.Message interface.
+func (e *Greet) UnmarshallBinary(data []byte) error {
+	sig, err := types.SignatureFromBytes(data)
+	if err != nil {
+		return err
+	}
+	e.Signature = sig
+	return nil
+}
